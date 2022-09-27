0e61ca9188975db08a50371e49b361ed3fb524e5
faultyserver <jonegeland@gmail.com>
2022-09-27 12:14:50 -0400

    add release configuration and set up github action

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule GraphicsAPI.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        graphics_api: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent]
+        ]
+      ]
     ]
   end
 
