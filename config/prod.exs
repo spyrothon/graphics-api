56d536af33ebd47c00afae7727869589f8601250
faultyserver <jonegeland@gmail.com>
2022-09-27 13:05:19 -0400

    use runtime.exs

--- config/prod.exs
+++ config/prod.exs
@@ -4,20 +4,4 @@ config :cors_plug,
   origin: ["*"],
   headers: ["*"]
 
-# Configure these in config/runtime.exs on the server
-#
-# config :graphics_api, GraphicsAPI.Repo, url: System.fetch_env!("API_DATABASE_URL")
-# config :graphics_api, :twitch,
-#   broadcaster_id: 55555,
-#   client_id: System.fetch_env!("TWITCH_CLIENT_ID"),
-#   client_secret: System.fetch_env!("TWITCH_CLIENT_SECRET"),
-#   scopes: [
-#     # Get info about bits on the channel
-#     "bits:read",
-#     # Manage stream title, create markers, etc.
-#     "channel:manage:broadcast",
-#     # Manage polls
-#     "channel:manage:polls",
-#     # Manage predictions
-#     "channel:manage:predictions"
-#   ]
+# Repo and Twitch configuration are managed in `runtime.exs`.
---
+++ config/runtime.exs
@@ -0,0 +1,20 @@
+import Config
+
+# These rely on the target server having these environment variables
+
+config :graphics_api, GraphicsAPI.Repo, url: System.fetch_env!("API_DATABASE_URL")
+
+config :graphics_api, :twitch,
+  broadcaster_id: 55555,
+  client_id: System.fetch_env!("TWITCH_CLIENT_ID"),
+  client_secret: System.fetch_env!("TWITCH_CLIENT_SECRET"),
+  scopes: [
+    # Get info about bits on the channel
+    "bits:read",
+    # Manage stream title, create markers, etc.
+    "channel:manage:broadcast",
+    # Manage polls
+    "channel:manage:polls",
+    # Manage predictions
+    "channel:manage:predictions"
+  ]
